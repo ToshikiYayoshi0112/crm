4b17838c50e3466f0335a6f14d7c0ef976b31594
Yayoshi <sagauyayoshi@gmail.com>
2019-06-29 18:42:43 +0900

    appを完成させた。 顧客情報を一覧表示する 顧客情報を追加できるようにした。

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from db import find_all_users, add_user
+
+
 def main():
     print("===== Welcome to CRM Application =====")
     print("[S]how all customers infomation")
@@ -10,8 +13,20 @@ def main():
     while command != "q":
         if command == "s":
             print("顧客一覧を表示します")
+
+            for user in find_all_users():
+                name = user[0]
+                age = user[1]
+
+                print(f"Name: {name} Age: {age}")
+
         elif command == "a":
             print("新規の顧客情報を追加します")
+            name = input("New customer's name? > ")
+            age = input("New customer's age? > ")
+
+            add_user(name, age)
+            print(f"Add new customer {name}")
         else:
             print(f"{command}: command not found")
 
--- db.py
+++ db.py
@@ -17,5 +17,35 @@ def init_db():
     conn.close()
 
 
+def find_all_users():
+    conn = sqlite3.connect("crm.sqlite")
+    cursor = conn.cursor()
+
+    sql = "SELECT* FROM customers"
+
+    results = cursor.execute(sql)
+
+    users = results.fetchall()
+
+    conn.commit()
+
+    conn.close()
+
+    return users
+
+
+def add_user(name, age):
+    conn = sqlite3.connect("crm.sqlite")
+    cursor = conn.cursor()
+
+    sql = "INSERT INTO customers (name, age) VALUES(?, ?)"
+
+    cursor.execute(sql, (name, age))
+
+    conn.commit()
+
+    conn.close()
+
+
 if __name__ == "__main__":
     init_db()
